dd07769c6f00f2edbfbf1ef48020666420103ff8
suicedek <handsomefox.hf@gmail.com>
2022-03-20 23:32:02 +0200

    Call Init() before starting the server.

--- cmd/start-server.go
+++ cmd/start-server.go
@@ -15,9 +15,10 @@ var startServerCmd = &cobra.Command{
 both using the CLI and the browser (using localhost:3000/s/ for making
 short links and localhost:3000/u/ for getting unrolled versions"`,
 	Run: func(cmd *cobra.Command, args []string) {
-		router := server.ShortURLServer{Address: "localhost:3000"}
+		s := server.ShortURLServer{Address: "localhost:3000"}
+		s.Init()
 		fmt.Println("Started the server at http://localhost:3000")
-		log.Fatal(router.Run())
+		log.Fatal(s.Run())
 	},
 }
 
