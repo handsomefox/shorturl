9b10d3c228cfa640eab695d55e4089dacbefa7b5
suicedek <handsomefox.hf@gmail.com>
2022-03-21 20:30:35 +0200

    Stop printing the key.

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 )
 
 func main() {
-	fmt.Println(os.Getenv("MONGO_KEY"))
 	s := server.ShortURLServer{Address: ":8080", DBKey: os.Getenv("MONGO_KEY")}
 	s.Init()
 	fmt.Println("Started the server at port 8080")
