52ea8bb32e9b243cdf322fb09d6965b5a8fa0599
suicedek <handsomefox.hf@gmail.com>
2022-03-21 19:35:14 +0200

    Make start-server command work with the database.

--- cmd/start-server.go
+++ cmd/start-server.go
@@ -2,23 +2,29 @@ package cmd
 
 import (
 	"fmt"
-	"github.com/spf13/cobra"
 	"log"
 	"shorturl/internal/server"
+
+	"github.com/spf13/cobra"
 )
 
 // start-serverCmd represents the start-server command
 var startServerCmd = &cobra.Command{
 	Use:   "start-server",
-	Short: "Starts the server required for the short links to work",
+	Short: "Starts the server required for the short links and connects to mongoDB.",
 	Long: `Starts an HTTP server which allows you to use the short links
-both using the CLI and the browser (using localhost:3000/s/ for making
-short links and localhost:3000/u/ for getting unrolled versions"`,
+		   using the browser (using localhost:3000/s/ for making
+  		   short links and localhost:3000/u/ for getting unrolled versions
+		   Connects to MongoDB if the argument provides the right password."`,
 	Run: func(cmd *cobra.Command, args []string) {
-		s := server.ShortURLServer{Address: "localhost:3000"}
-		s.Init()
-		fmt.Println("Started the server at http://localhost:3000")
-		log.Fatal(s.Run())
+		if len(args) == 1 {
+			s := server.ShortURLServer{Address: "localhost:3000", DBKey: args[0]}
+			s.Init()
+			fmt.Println("Started the server at http://localhost:3000")
+			log.Fatal(s.Run())
+		} else {
+			fmt.Println("Please provide mongodb key as an argument, or no arguments for the local file version.")
+		}
 	},
 }
 
