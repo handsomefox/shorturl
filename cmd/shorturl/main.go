01f5bec8944010cbf0cf346c0af46a80f5dba0d6
suicedek <handsomefox.hf@gmail.com>
2022-04-30 18:27:48 +0300

    Better port and database key detection

--- cmd/shorturl/main.go
+++ cmd/shorturl/main.go
@@ -7,16 +7,22 @@ import (
 	"shorturl/pkg/server"
 )
 
-func main() {
+func getPort() string {
 	port := os.Getenv("PORT")
-
 	if len(port) == 0 {
 		port = ":8080"
 	} else {
 		port = ":" + port
 	}
+	return port
+}
 
+func main() {
+	port := getPort()
 	key := os.Getenv("MONGO_KEY")
+	if len(key) == 0 {
+		log.Fatal("no database key was provided")
+	}
 
 	s := server.ShortURLServer{Address: port, DBKey: key}
 	err := s.Init()
